276fd364d3fb43e188064294ec291445ddd03cd0
Nancy El-Sharkawi <elsharkawinancy@gmail.com>
2020-06-26 18:35:18 +0300

    speedtest5 is ready

---
+++ Development/js/scripts.js
@@ -0,0 +1,11 @@
+let instructor = {
+  name: "Menawer",
+  courses: ["JavaScript", "React", "PHP"],
+  githubAccount: {
+    username: "careless10",
+    numberOfRepos: 200,
+  },
+  sayHello: function () {
+    console.log("Hi, I'm Menawer");
+  },
+};
