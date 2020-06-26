82e99c584d17c036c4bb1d42e8d8eeceeeb08d34
Nancy El-Sharkawi <elsharkawinancy@gmail.com>
2020-06-26 18:31:50 +0300

    solution

---
+++ Development/js/scripts.js
@@ -0,0 +1,17 @@
+let instructor = {
+  name: "Menawer",
+  courses: ["JavaScript", "React", "PHP"],
+  githubAccount: {
+    username: "careless10",
+    numberOfRepos: 200,
+  },
+  sayHello: function () {
+    console.log("Hi, I'm Menawer");
+  },
+};
+
+// solution
+console.log(instructor["name"]);
+console.log(instructor["courses"][0]);
+console.log(instructor["githubAccount"]["username"]);
+instructor["sayHello"]();
